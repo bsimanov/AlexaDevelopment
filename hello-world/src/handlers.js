bbafa7b8f8572ff8ca501574b3a451eb02689efc
Boris S <bsimanov@gmail.com>
2016-08-05 21:55:08 -0400

    Added all handlers & tests

--- hello-world/src/handlers.js
+++ hello-world/src/handlers.js
@@ -1,17 +1,38 @@
+var HelpIntent = require('./intents/HelpIntent')
+var ActionIntent = require('./intents/ActionIntent')
+const constants = require('./resources/constants')
+
 module.exports = {
 
     'LaunchRequest': function () {
-        //this.emit('SayHello');
-        this.emit(':tell', 'Launched Hello World...')
+        this.emit(':ask', constants.outputSpeech.StartSession);
     },
 
-    'HelloWorldIntent': function () {
-        this.emit('SayHello')
+    "ActionIntent": function () {
+        var request = this.event.request
+        var slots = request.intent.slots
+        var action = "unknown"
+        if (slots && slots.ActionType && slots.ActionType.value)
+            action = slots.ActionType.value
+
+        if (action == "unknown")
+            speechText = constants.outputSpeech.ActionIntentDefault
+        else
+            speechText = ActionIntent.getSpeech(action)
+        
+        this.emit(':ask', speechText)
     },
 
-    'SayHello': function () {
-        //this.emit(':tell', 'Hello World!');
-        this.emit(':ask', 'What would you like to do?', 'Please say that again?');
+    "AMAZON.HelpIntent": function (prefixMessage) {
+        this.emit(':ask', constants.outputSpeech.HelpIntent)
+    },
+
+    "AMAZON.StopIntent": function () {
+        this.emit(':tell', constants.outputSpeech.StopSession)
+    },
+
+    "AMAZON.CancelIntent": function () {
+        this.emit(':tell', constants.outputSpeech.StopSession)
     }
 
 };
--- hello-world/test/handlers.test.js
+++ hello-world/test/handlers.test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect
 const Alexa = require('alexa-sdk');
 const handlers = require('../src/handlers')
+const constants = require('../src/resources/constants')
 
 const applicationId = 1337
 
@@ -43,7 +44,8 @@ describe('handlers', function () {
   it('check all handlers', function () {
     expect(handlers).to.be.an.object
     var handlerNames = Object.keys(handlers);
-    expect(handlerNames).to.deep.equal(["LaunchRequest", "HelloWorldIntent", "SayHello"])
+    const expectHandlers = ["LaunchRequest", "ActionIntent", "AMAZON.HelpIntent", "AMAZON.StopIntent", "AMAZON.CancelIntent"]
+    expect(handlerNames).to.deep.equal(expectHandlers)
     for (var i = 0; i < handlerNames.length; i++) {
       expect(typeof (handlers[handlerNames[i]])).to.equal('function')
     }
@@ -55,35 +57,60 @@ describe('handlers', function () {
     }
 
     var response = callAlexa(event)
+    expect(response.shouldEndSession).to.be.false
+    expect(response.outputSpeech.ssml).to.equal('<speak> ' + constants.outputSpeech.StartSession + ' </speak>')
+  })
+
+  it('ActionIntent (Default)', function () {
+    event.request = {
+      type: 'IntentRequest',
+      intent: {
+        name: 'ActionIntent'
+      }
+    }
+
+    var response = callAlexa(event)
+    expect(response.shouldEndSession).to.be.false
+    expect(response.outputSpeech.ssml).to.equal('<speak> ' + constants.outputSpeech.ActionIntentDefault + ' </speak>')
+    //expect(response.reprompt.outputSpeech.ssml).to.equal('<speak> Please say that again? </speak>')
+  })
+
+  it('AMAZON.HelpIntent', function() {
+    event.request = {
+      type: 'IntentRequest',
+      intent: {
+        name: 'AMAZON.HelpIntent'
+      }
+    }
+
+    var response = callAlexa(event)
+    expect(response.shouldEndSession).to.be.false
+    expect(response.outputSpeech.ssml).to.equal('<speak> ' + constants.outputSpeech.HelpIntent + ' </speak>')
+  })
+
+  it('AMAZON.StopIntent', function() {
+    event.request = {
+      type: 'IntentRequest',
+      intent: {
+        name: 'AMAZON.StopIntent'
+      }
+    }
+
+    var response = callAlexa(event)
     expect(response.shouldEndSession).to.be.true
-    expect(response.outputSpeech.ssml).to.equal('<speak> Launched Hello World... </speak>')
+    expect(response.outputSpeech.ssml).to.equal('<speak> ' + constants.outputSpeech.StopSession + ' </speak>')
   })
 
-  it('HelloWorldIntent', function () {
+  it('AMAZON.CancelIntent', function() {
     event.request = {
       type: 'IntentRequest',
       intent: {
-        name: 'HelloWorldIntent'
+        name: 'AMAZON.CancelIntent'
       }
     }
 
     var response = callAlexa(event)
-    expect(response.shouldEndSession).to.be.false
-    expect(response.outputSpeech.ssml).to.equal('<speak> What would you like to do? </speak>')
-    expect(response.reprompt.outputSpeech.ssml).to.equal('<speak> Please say that again? </speak>')
-  })
-
-  it('SayHello', function () {
-    event.request = {
-      type: 'IntentRequest',
-      intent: {
-        name: 'SayHello'
-      }
-    }
-
-    var response = callAlexa(event)
-    expect(response.shouldEndSession).to.be.false
-    expect(response.outputSpeech.ssml).to.equal('<speak> What would you like to do? </speak>')
-    expect(response.reprompt.outputSpeech.ssml).to.equal('<speak> Please say that again? </speak>')
+    expect(response.shouldEndSession).to.be.true
+    expect(response.outputSpeech.ssml).to.equal('<speak> ' + constants.outputSpeech.StopSession + ' </speak>')
   })
 })
